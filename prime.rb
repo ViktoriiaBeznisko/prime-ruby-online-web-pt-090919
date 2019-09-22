7cb04c1d84ef6c8dc8dda74210e75ee0614c8056
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-22 21:46:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(number)
   start_count = 2
   if number > 1
-    range = (start_count..number-1).to_a
-    range.none? do |num_to_test| 
+    new_numbers = (start_count..number-1).to_a
+    new_numbers.none? do |num_to_test| 
       number % num_to_test == 0
     end
   else
